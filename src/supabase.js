ca5cb7ade72d1cebff08c5a8e68bc97c8d05fc94
Nikolas Cruz <onikolascruz@gmail.com>
2026-02-03 10:41:59 -0300

    subir supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-// import.meta.env é o comando mágico do Vite para ler o arquivo .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+// --- ADICIONE ISTO AQUI PARA TESTAR ---
+console.log('https://olcmgvpddtfgujpajanz.supabase.co', supabaseUrl)
+console.log('sb_publishable_GwidpJlXXDXGXUBrzPCj3A_zqlj5adA', supabaseKey)
+// -------------------------------------
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
